768420e29e1b6b9f583dbc0a992bc9874d6de100
fcracker79 <fcracker79@gmail.com>
2020-01-23 21:35:04 +0100

    First commit: just a bare project structure

---
+++ backend/src/main/java/io/mirko/alexa/home/raspberry/SubscribeDeviceLambda.java
@@ -0,0 +1,19 @@
+package io.mirko.alexa.home.raspberry;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+@Named("test")
+public class SubscribeDeviceLambda implements RequestHandler<InputObject, OutputObject> {
+
+    @Inject
+    ProcessingService service;
+
+    @Override
+    public OutputObject handleRequest(InputObject input, Context context) {
+        return service.process(input).setRequestId(context.getAwsRequestId());
+    }
+}
