e53eae1441810c41482848ec8bb1c15556c0e847
fcracker79 <fcracker79@gmail.com>
2020-01-30 21:02:15 +0100

    Fixed error with temporary stuff.

--- backend/copy_native.sh
+++ backend/copy_native.sh
@@ -3,5 +3,5 @@ set -e
 
 TERRAFORM_PATH="$HOME/dev/terraform/home_terraform/modules/raspberry_lambda"
 BUILD_DIR="target"
-mvn clean install -Pnative -Dnative-image.docker-build=true -Dquarkus.native.enable-jni=true
+mvn clean install -Pnative -Dnative-image.docker-build=true -Dquarkus.native.enable-jni=true 
 cp $BUILD_DIR/function.zip $TERRAFORM_PATH/raspberry_backend.zip
