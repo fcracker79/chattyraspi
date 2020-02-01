0015f2fc0cb08953288d611393b5732d92760639
fcracker79 <fcracker79@gmail.com>
2020-02-01 17:51:07 +0100

    Added sample code from Alexa GitHub examples to support discovery

--- skill/src/main/java/io/mirko/lambda/QuarkusDelegateStreamSkillLambda.java
+++ skill/src/main/java/io/mirko/lambda/QuarkusDelegateStreamSkillLambda.java
@@ -17,7 +17,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 @Named("lambda_entry_point")
@@ -54,18 +54,28 @@ public class QuarkusDelegateStreamSkillLambda implements RequestHandler<Map, Map
     @Override
     public Map handleRequest(Map request, Context context) {
         System.out.format("************ Handling request %s\n", request);
-        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+        Optional<String> namespace = getFromMap(request, "directive.header.namespace");
+        Optional<String> header = getFromMap(request, "directive.header.name");
+        if ("Alexa.Discovery".equals(namespace.orElse(null)) && "Discover".equals(header.orElse(null))) {
+            return discovery(request, context);
+        }
+        throw new IllegalStateException(String.format("Unexpected %s", request));
+    }
+
+    private Map<String, Object> discovery(Map<String, Object> request, Context context) {
+        System.out.println("Found Alexa.Discovery Namespace");
+        AlexaResponse ar = new AlexaResponse("Alexa.Discovery", "Discover.Response");
+        String capabilityAlexa = ar.CreatePayloadEndpointCapability("AlexaInterface", "Alexa", "3", null);
+        String capabilityAlexaPowerController = ar.CreatePayloadEndpointCapability("AlexaInterface", "Alexa.PowerController", "3", "{\"supported\": [ { \"name\": \"powerState\" } ] }");
+        String capabilities = "[" + capabilityAlexa + ", " + capabilityAlexaPowerController + "]";
+        // TODO add real devices
+        ar.AddPayloadEndpoint("Sample Switch", "sample-switch-01", capabilities);
+        final String strResponse = ar.toString();
         try {
-            delegate.handleRequest(streamFromRequest(request), os, context);
-            System.out.format("************ Got response %s\n", new String(os.toByteArray()));
-            return JSON_OBJECT_MAPPER.readerFor(Map.class).readValue(os.toByteArray());
+            return JSON_OBJECT_MAPPER.readValue(strResponse, Map.class);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } catch(Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
         }
-
     }
 
     private static <T> Stream<T> getBeans(Type t) {
@@ -73,5 +83,19 @@ public class QuarkusDelegateStreamSkillLambda implements RequestHandler<Map, Map
         //noinspection unchecked
         return (Stream<T>) bm.getBeans(t).stream()
                 .map(b -> bm.getReference(b, t, bm.createCreationalContext(b)));
+    }
+
+    private <T> Optional<T> getFromMap(Map m, String path) {
+        Object lastValue = m;
+        for (String field : path.split("\\.")) {
+            if (lastValue == null) {
+                return Optional.empty();
+            }
+            lastValue = ((Map) lastValue).get(field);
+        }
+        if (lastValue == null) {
+            return Optional.empty();
+        }
+        return Optional.of((T) lastValue);
     }
 }
--- skill/src/test/java/io/mirko/impl/SmokeTest.java
+++ skill/src/test/java/io/mirko/impl/SmokeTest.java
@@ -1,15 +1,10 @@
 package io.mirko.impl;
 
 
-import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import io.mirko.lambda.QuarkusDelegateStreamSkillLambda;
-import io.mirko.lambda.handlers.CancelandStopIntentHandler;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import javax.enterprise.inject.spi.CDI;
 
 @QuarkusTest
 public class SmokeTest {
@@ -21,9 +16,9 @@ public class SmokeTest {
         //         17,
         //         CDI.current().getBeanManager().getBeans(RequestHandler.class).size()
         // );
-        Assertions.assertEquals(
-                1,
-                CDI.current().getBeanManager().getBeans(CancelandStopIntentHandler.class).size()
-        );
+        // Assertions.assertEquals(
+        //         1,
+        //         CDI.current().getBeanManager().getBeans(CancelandStopIntentHandler.class).size()
+        // );
     }
 }
