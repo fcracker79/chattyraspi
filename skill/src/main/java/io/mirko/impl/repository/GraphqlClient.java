a79cb41a9ca968461e34029862b2909746f0aaf8
fcracker79 <fcracker79@gmail.com>
2020-03-04 23:54:32 +0100

    Fixed GraphQL client.

--- skill/src/main/java/io/mirko/impl/repository/GraphqlClient.java
+++ skill/src/main/java/io/mirko/impl/repository/GraphqlClient.java
@@ -1,5 +1,6 @@
 package io.mirko.impl.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 
@@ -9,6 +10,8 @@ import javax.inject.Named;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +19,7 @@ import java.util.Map;
 @Named
 @ApplicationScoped
 public class GraphqlClient {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     @ConfigProperty(name="io.mirko.alexa.home.raspberry.graphql_uri")
     String graphqlUri;
 
@@ -34,7 +38,12 @@ public class GraphqlClient {
                     String.format("Status %s: %s", response.getStatus(), response.getEntity())
             );
         }
-        return (Map<String, Object>) response.getEntity();
+        final InputStream is = (InputStream) response.getEntity();
+        try {
+            return (Map<String, Object>) OBJECT_MAPPER.readValue(is, Map.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Map<String, Object> query(String query, Map<String, Object> variables) {
