7ce6d713384bd3d177f68d49cbe6fa663de6fe98
fcracker79 <fcracker79@gmail.com>
2020-03-05 19:30:01 +0100

    Added some logs. Completed mark executed and mark failed commands.

--- clients/generic/raspi_alexa/client.py
+++ clients/generic/raspi_alexa/client.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import time
 from concurrent.futures.thread import ThreadPoolExecutor
 from urllib.parse import urlparse
@@ -24,6 +25,29 @@ class DeviceIdClient:
         self._device_id = device_id
         self.on_turn_on = _do_nothing
         self.on_turn_off = _do_nothing
+        self._logger = logging.getLogger('client.raspi.alexa.mirko.io')
+
+    @property
+    def device_id(self) -> str:
+        return self._device_id
+
+    def _info(self, s: str, *args, **kwargs):
+        self._log(self._logger.info, s, *args)
+
+    def _debug(self, s: str, *args):
+        self._log(self._logger.debug, s, *args)
+
+    def _warning(self, s: str, *args):
+        self._log(self._logger.warning, s, *args)
+
+    def _error(self, s: str, *args):
+        self._log(self._logger.error, s, *args)
+
+    def _exception(self, s: str, *args):
+        self._log(self._logger.exception, s, *args)
+
+    def _log(self, log_function, s: str, *args, **kwargs):
+        log_function('Device %s: {}'.format(s), self._device_id, *args)
 
     def _query(self, q: str):
         response = requests.post(_ROOT_URL, json={'query': q}, headers=self._headers)
@@ -61,7 +85,7 @@ class DeviceIdClient:
         try:
             r.raise_for_status()
         except Exception:
-            print('ERROR!!!!!!!!!!!!', r.content)
+            self._exception('Could not subscribe to device commands, status %s, response %s', r.status_code, r.content)
             raise
         data = r.json()
         # if data.get('errors'):
@@ -70,18 +94,39 @@ class DeviceIdClient:
         ws_url = r.json()['extensions']['subscription']['mqttConnections'][0]['url']
         topic = r.json()['extensions']['subscription']['mqttConnections'][0]['topics'][0]
 
-        def on_message(client, userdata, msg):
-            command = json.loads(msg.payload.decode())
-            command_id = command['data']['onCommandCreated']['commandId']
-            if command_id == 'turnOn':
-                self.on_turn_on()
-            elif command_id == 'turnOff':
-                self.on_turn_off()
-            else:
-                raise ValueError('Unexpected command {}, payload {}'.format(msg.payload['command'], msg))
+        # noinspection PyUnusedLocal
+        def on_message(_client, _userdata, msg):
+            #  {
+            #      "data": {
+            #          "onCommandCreated": {
+            #              "commandId": "3cb6a547-1fea-4034-a98e-9e2b1c17aed6",
+            #              "deviceId": "device_001",
+            #              "command": "turnOn",
+            #              "arguments": [],
+            #              "status": 1,
+            #              "__typename": "Command"
+            #          }
+            #      }
+            #  }
+            command_payload = json.loads(msg.payload.decode())
+            command = command_payload['data']['onCommandCreated']['command']
+            command_id = command_payload['data']['onCommandCreated']['commandId']
+            # noinspection PyBroadException
+            try:
+                if command == 'turnOn':
+                    self.on_turn_on()
+                elif command == 'turnOff':
+                    self.on_turn_off()
+                else:
+                    raise ValueError('Unexpected command {}, payload {}'.format(msg.payload['command'], msg))
+                self._command_executed(command_id)
+            except Exception:
+                self._exception('Could not process command %s (%s)', command, command_id)
+                self._command_failed(command_id)
 
-        def on_connect(client, userdata, flags, rc):
-            print("Connected with result code " + str(rc))
+        # noinspection PyUnusedLocal
+        def on_connect(_client, _userdata, flags, rc):
+            self._debug('Connected to subscription topic')
             client.subscribe(topic)
         urlparts = urlparse(ws_url)
 
@@ -96,9 +141,46 @@ class DeviceIdClient:
         client.ws_set_options(path="{}?{}".format(urlparts.path, urlparts.query), headers=headers)
         client.tls_set()
 
-        print("trying to connect now....")
+        self._debug('trying to connect now....')
         client.connect(urlparts.netloc, 443)
-        client.loop_forever()
+        try:
+            client.loop_forever()
+        finally:
+            client.disconnect()
+
+    def _command_executed(self, command_id: str):
+        self._info('Marking command %s as executed', command_id)
+        self._mutation(
+            '''
+            mutation commandExecuted($commandId: ID!) {
+                  markCommandAsExecuted(id: $commandId) {
+                    commandId
+                    command
+                    deviceId
+                  }
+                }
+            ''',
+            {
+                'commandId': command_id
+            }
+        )
+
+    def _command_failed(self, command_id: str):
+        self._error('Marking command %s as FAILED', command_id)
+        self._mutation(
+            '''
+            mutation commandExecuted($commandId: ID!) {
+                  markCommandAsFailed(id: $commandId) {
+                    commandId
+                    command
+                    deviceId
+                  }
+                }
+            ''',
+            {
+                'commandId': command_id
+            }
+        )
 
 
 class Client:
@@ -107,6 +189,7 @@ class Client:
             conf['device_id']: DeviceIdClient(conf['device_id'], conf['openid_token'])
             for conf in configuration.get_configuration()['Devices']
         }
+        self._logger = logging.getLogger('client.raspi.alexa.mirko.io')
 
     def set_on_turn_on(self, device_id: str, fun: callable):
         self._clients_by_device_id[device_id].on_turn_on = fun
@@ -116,13 +199,18 @@ class Client:
 
     def listen(self):
         executor = ThreadPoolExecutor(max_workers=len(self._clients_by_device_id))
+        # noinspection PyTypeChecker
         for client in self._clients_by_device_id.values():
             executor.submit(self._safe_submit, client)
 
     def _safe_submit(self, client: DeviceIdClient):
         while True:
+            self._logger.info('Start listening to device %s commands....', client.device_id)
+            # noinspection PyBroadException
             try:
                 client.listen()
             except Exception:
-                pass
+                self._logger.exception(
+                    'Error listening to device %s commands. Retrying in 1 second...',
+                    client.device_id)
             time.sleep(1)
