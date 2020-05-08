2c4a9a457e5af77d105513018c3e531b6a8565d1
fcracker79 <fcracker79@gmail.com>
2020-05-08 22:59:20 +0200

    Debug level only for io.mirko.

--- skill/src/test/java/io/mirko/impl/SmokeTest.java
+++ skill/src/test/java/io/mirko/impl/SmokeTest.java
@@ -5,13 +5,21 @@ import io.mirko.lambda.QuarkusDelegateStreamSkillLambda;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @QuarkusTest
 public class SmokeTest {
     @Test
-    @Disabled
     public void smokeTest() {
         new QuarkusDelegateStreamSkillLambda();
+        Logger logger = LoggerFactory.getLogger("io.mirko.test1");
+        logger.info("io.mirko info");
+        logger.debug("io.mirko debug");
+
+        logger = LoggerFactory.getLogger("io.quarkus.test1");
+        logger.info("io.quarkus info");
+        logger.debug("io.quarkus debug");
         // Assertions.assertEquals(
         //         17,
         //         CDI.current().getBeanManager().getBeans(RequestHandler.class).size()
