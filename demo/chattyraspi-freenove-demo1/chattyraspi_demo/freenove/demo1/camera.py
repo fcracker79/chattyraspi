1bc216d4031740ecc7561f4de4d24bfc0412ca72
fcracker79 <fcracker79@gmail.com>
2020-07-03 17:42:16 +0200

    Fixed PWM pulse. Credits @Freenove.

--- demo/chattyraspi-freenove-demo1/chattyraspi_demo/freenove/demo1/camera.py
+++ demo/chattyraspi-freenove-demo1/chattyraspi_demo/freenove/demo1/camera.py
@@ -3,7 +3,9 @@ import logging
 import wiringpi
 from chattyraspi.client import ThermostatMode
 
-
+_OFFSET_MS = 3
+_SERVO_MIN_MS = _OFFSET_MS + 5
+_SERVO_MAX_MS = _OFFSET_MS + 25
 _SERVO_PIN = 1
 _LOGGER = logging.getLogger('servomotor')
 
@@ -20,9 +22,14 @@ def stop_camera():
 def set_degree(degree: float):
     degree = int(min(180.0, max(0.0, degree)))
     _LOGGER.info('wiringpi.softPwmWrite(%s, %s)', _SERVO_PIN, degree)
+    degree = map_angle(degree, 0, 180, _SERVO_MIN_MS, _SERVO_MAX_MS)
     wiringpi.softPwmWrite(_SERVO_PIN, degree)
 
 
 def set_mode(mode: ThermostatMode):
     _LOGGER.info('TODO IMPLEMENT MODE for %s', mode)
     pass
+
+
+def map_angle(value: int, from_low: int, from_high: int, to_low: int, to_high: int):
+    return (to_high-to_low)*(value-from_low) / (from_high-from_low) + to_low
