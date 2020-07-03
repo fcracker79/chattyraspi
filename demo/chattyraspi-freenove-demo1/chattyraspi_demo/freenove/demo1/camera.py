e6346dbe530108b4c90e95e39c6dccf8f2455b1e
fcracker79 <fcracker79@gmail.com>
2020-07-03 17:27:46 +0200

    Added camera support.

---
+++ demo/chattyraspi-freenove-demo1/chattyraspi_demo/freenove/demo1/camera.py
@@ -0,0 +1,22 @@
+import wiringpi
+from chattyraspi.client import ThermostatMode
+
+
+_SERVO_PIN = 1
+
+
+def start_camera():
+    wiringpi.softPwmCreate(_SERVO_PIN, 0, 200)
+
+
+def stop_camera():
+    pass
+
+
+def set_degree(degree: float):
+    degree = min(180.0, max(0.0, degree))
+    wiringpi.softPwmWrite(_SERVO_PIN, degree)
+
+
+def set_mode(mode: ThermostatMode):
+    pass
--- demo/chattyraspi-freenove-demo1/chattyraspi_demo/freenove/demo1/main.py
+++ demo/chattyraspi-freenove-demo1/chattyraspi_demo/freenove/demo1/main.py
@@ -7,14 +7,14 @@ import typing
 from chattyraspi.client import Client, ThermostatMode
 from chattyraspi.device import DevicesConfiguration
 
-from chattyraspi_demo.freenove.demo1 import lights
-
+from chattyraspi_demo.freenove.demo1 import lights, camera
 
 LIGHTS = {
     '4e3c7b4a-fc25-4779-8923-2962aae068d2': lights.Light.LUCE0,
     '76cbaa37-2228-48f3-bb01-ae26033ede5d': lights.Light.LUCE1,
     '940b2d63-94d9-4646-bf3a-d525c9d3a56a': lights.Light.LUCE2
 }
+CAMERA = '79895ffc-3524-4af0-8233-a5b4277275dd'
 
 
 @click.command()
@@ -37,12 +37,18 @@ def test_devices(
     def _turn_on(device_id: str):
         print('Device {} turned ON'.format(device_id))
         statuses[device_id] = True
-        lights.turn_on_light(LIGHTS[device_id])
+        if device_id in LIGHTS:
+            lights.turn_on_light(LIGHTS[device_id])
+        else:
+            camera.start_camera()
 
     def _turn_off(device_id: str):
         print('Device {} turned OFF'.format(device_id))
         statuses[device_id] = False
-        lights.turn_off_light(LIGHTS[device_id])
+        if device_id in LIGHTS:
+            lights.turn_off_light(LIGHTS[device_id])
+        else:
+            camera.stop_camera()
 
     def _fetch_is_power_on(device_id: str) -> bool:
         print('Device {} requested power status'.format(device_id))
@@ -74,6 +80,8 @@ def test_devices(
     def _on_set_temperature(device_id: str, temperature: float):
         print('Device {} set temperature at {}'.format(device_id, temperature))
         temperatures[device_id] = temperature
+        if device_id == CAMERA:
+            camera.set_degree(temperature)
 
     def _on_adjust_temperature(device_id: str, temperature: float):
         print('Device {} adjust temperature by {}'.format(device_id, temperature))
@@ -82,6 +90,8 @@ def test_devices(
     def _on_set_thermostat_mode(device_id: str, thermostat_mode: ThermostatMode):
         print('Device {} set thermostat_mode {}'.format(device_id, thermostat_mode))
         thermostat_modes[device_id] = thermostat_mode
+        if device_id == CAMERA:
+            camera.set_mode(thermostat_mode)
 
     for device_id in map(lambda d: d['device_id'], config.get_configuration()['Devices']):
         statuses[device_id] = False
