81825eeb25f9c41200b693133f11832adbba7a8e
fcracker79 <fcracker79@gmail.com>
2020-07-03 17:35:37 +0200

    Some logs.

--- demo/chattyraspi-freenove-demo1/chattyraspi_demo/freenove/demo1/camera.py
+++ demo/chattyraspi-freenove-demo1/chattyraspi_demo/freenove/demo1/camera.py
@@ -1,11 +1,15 @@
+import logging
+
 import wiringpi
 from chattyraspi.client import ThermostatMode
 
 
 _SERVO_PIN = 1
+_LOGGER = logging.getLogger('servomotor')
 
 
 def start_camera():
+    _LOGGER.info('wiringpi.softPwmCreate(%s, 0, 200)', _SERVO_PIN)
     wiringpi.softPwmCreate(_SERVO_PIN, 0, 200)
 
 
@@ -14,9 +18,11 @@ def stop_camera():
 
 
 def set_degree(degree: float):
-    degree = min(180.0, max(0.0, int(degree)))
+    degree = int(min(180.0, max(0.0, degree)))
+    _LOGGER.info('wiringpi.softPwmWrite(%s, %s)', _SERVO_PIN, degree)
     wiringpi.softPwmWrite(_SERVO_PIN, degree)
 
 
 def set_mode(mode: ThermostatMode):
+    _LOGGER.info('TODO IMPLEMENT MODE for %s', mode)
     pass
